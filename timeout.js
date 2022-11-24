7e2c6d3860c50e5a0d4599c68436e2cd1b39a53a
EpikHriday <hriday.goswami1821@gmail.com>
2022-11-24 18:26:45 +0530

    Today's session

---
+++ timeout.js
@@ -0,0 +1,11 @@
+// setTimeout(()=> {
+//     console.log("Hello world");
+// }, 3000)
+// function hello() {
+//     console.log("Hello");
+// }
+// setTimeout(hello, 3000);
+
+setTimeout(function() {
+    console.log("Hello");
+}, 3000);
